0ca1bcc6a9fdaf7f0b0938f0825ef8e1e15c3be0
ddab1125 <trk1125@gmail.com>
2022-11-19 11:15:28 +0100

    Revert "Revert "Initial Commit""
    This reverts commit 25746ac4b88fc213b5f0a2dcb14d6ce4d77a4daa.

---
+++ src/test/java/pl/coderslab/dogmeat/DogmeatApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.dogmeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogmeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
